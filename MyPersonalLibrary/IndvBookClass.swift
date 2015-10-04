b7a3aba49621eb9c7160e7c62d4ed0c564058a46
Melissa W <mwebdesign@icloud.com>
2015-10-04 01:34:22 -0400

    Copy in basic Book class from previous library attempt

---
+++ MyPersonalLibrary/IndvBookClass.swift
@@ -0,0 +1,30 @@
+//
+//  IndvBookClass.swift
+//  MyPersonalLibrary
+//
+//  Created by Melissa on 10/4/15.
+//  Copyright © 2015 Melissa Webster. All rights reserved.
+//
+
+import Foundation
+
+class Book {
+    var title: String?
+    var author: String?
+    var lengthInPages: Int?
+    var genre: String?
+    
+    func newBook() {
+        
+        
+    }
+    
+    func editBook() {
+        
+    }
+    
+    func deleteBook() {
+        
+    }
+}
+
